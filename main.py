14cd39f0b907fa22b8add3a7a0505a93b0a4578c
alexal1 <5740235@gmail.com>
2023-03-30 22:50:35 +0400

    Add CORS

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 from logger import logger
 from routes import bp
 
 app = Flask(__name__)
+cors = CORS(app)
 app.logger.addHandler(logger)
 app.register_blueprint(bp)
 
